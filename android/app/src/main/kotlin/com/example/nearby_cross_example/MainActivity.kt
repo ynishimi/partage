cbfe4f7784a8b620e3599455821c6b2022d03a1b
Yuki Nishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-08-14 14:01:16 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/nearby_cross_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ynishimi.partage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:nearby_cross/constants/app.dart';
+import 'package:nearby_cross/models/connections_manager_model.dart';
+import 'package:nearby_cross/modules/authentication/experimental/experimental_auth_manager.dart';
+import 'package:partage/models/example_authentication_manager.dart';
+import 'package:partage/screens/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+  const appMode =
+      String.fromEnvironment("APP_MODE", defaultValue: 'experimental');
+  Logger().i("Starting app in $appMode mode");
+
+  if (AppMode.values.byName(appMode) == AppMode.experimental) {
+    // In experimental mode we let the devices create/retrieve their own Public/Private Keys
+    var experimentalAuthManager = ExperimentalAuthManager();
+    experimentalAuthManager.setIdentifier('test_experimental');
+    ConnectionsManager(authenticationManager: experimentalAuthManager);
+  } else {
+    // If we are not in experimental mode, we will force devices to use a Public/Private Keys
+    // declared in ExampleAuthenticationManager
+    const profile = String.fromEnvironment("PROFILE", defaultValue: 'DOCENTE');
+    Logger().i("Setting profile: $profile");
+
+    var exampleAuthManager = ExampleAuthenticationManager();
+    exampleAuthManager.setIdentifier(exampleAuthManager.devices[profile]!);
+    ConnectionsManager(authenticationManager: exampleAuthManager);
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MainScreen());
+  }
+}
